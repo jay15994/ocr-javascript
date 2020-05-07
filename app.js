6742b6c3a98379b19cc490d42624d4350c37a26f
Jay Vaghasiya <41486390+jay15994@users.noreply.github.com>
2020-05-07 12:53:22 +0530

    Image to Text/ORC using ocrad.js

---
+++ app.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var OCRAD = require('ocrad.js');
+var Canvas = require('canvas');
+
+var Image = Canvas.Image;
+
+fs.readFile(__dirname + '/test.png', function (err, src) {
+  if (err) {
+    throw err;
+  }
+  var img = new Image();
+  img.src = src;
+  var canvas = new Canvas.createCanvas(img.width, img.height);
+  var ctx = canvas.getContext('2d');
+  ctx.drawImage(img, 0, 0, img.width, img.height);
+  console.log(OCRAD(canvas));
+});
